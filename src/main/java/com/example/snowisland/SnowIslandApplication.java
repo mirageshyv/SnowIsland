da37abdf991d058bd9c1c4cacc5d040af229a8a5
mirageshyv <695390489@qq.com>
2026-04-26 20:02:41 +0800

    text

---
+++ src/main/java/com/example/snowisland/SnowIslandApplication.java
@@ -0,0 +1,13 @@
+package com.example.snowisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowIslandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowIslandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snowisland/SnowIslandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snowisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowIslandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
